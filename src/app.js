4c6977331b06f19acc4c806567394f0090546b9f
Pedro Castilho <pedro@castilho.in>
2020-03-19 17:05:02 -0300

    Set hard coded tasks and view

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const todos = require('./todos');
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', { todos });
 });
 module.exports = app;
