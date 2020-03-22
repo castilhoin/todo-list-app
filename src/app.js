566c23bea5fe670e632a3223049bad0a67caedeb
Pedro Castilho <pedro@castilho.in>
2020-03-22 16:27:43 -0300

    Handling db errors

--- src/app.js
+++ src/app.js
@@ -11,33 +11,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 // index - show tasks
 app.get('/', async (req, res, next) => {
-  const query = await db.query("SELECT * FROM tasks");
-  const tasks = query.rows;
-  res.render('index', { tasks });
+  try {
+    const query = await db.query("SELECT * FROM tasks");
+    const tasks = query.rows;
+    res.render('index', { tasks });
+  } catch(err) {
+    console.log('Error: ', err);
+    res.render('error', { err });
+  };
 });
 
 // create task
 app.post('/', async (req, res, next) => {
-  const name = req.body.newTask;
-  await db.query("INSERT INTO tasks (name, completed) VALUES ($1, $2)", [name, false]);
-  res.redirect('/');
+  try {
+    const name = req.body.newTask;
+    await db.query("INSERT INTO tasks (name, completed) VALUES ($1, $2)", [name, false]);
+    res.redirect('/');
+  } catch(err) {
+    console.log('Error: ', err);
+    res.render('error', { err });
+  };
 });
 
 // edit task
 app.get('/:id/edit', async (req, res, next) => {
-  const id = req.params.id;
-  const query = await db.query("SELECT * FROM tasks WHERE id = $1", [id]);
-  const row = query.rows[0];
-  res.render('edit', { task: row });
+  try {
+    const id = req.params.id;
+    const query = await db.query("SELECT * FROM tasks WHERE id = $1", [id]);
+    const row = query.rows[0];
+    res.render('edit', { task: row });
+  } catch(err) {
+    console.log('Error: ', err);
+    res.render('error', { err });
+  };
 });
 
 // update task
 app.put('/:id', async (req, res, next) => {
-  const id = req.params.id;
-  const name = req.body.taskName;
-  const completed = (req.body.completed === 'completed') ? true : false;
-  await db.query("UPDATE tasks SET name = $2, completed = $3 WHERE id = $1", [id, name, completed]);
-  res.redirect('/');
+  try {
+    const id = req.params.id;
+    const name = req.body.taskName;
+    const completed = (req.body.completed === 'completed') ? true : false;
+    await db.query("UPDATE tasks SET name = $2, completed = $3 WHERE id = $1", [id, name, completed]);
+    res.redirect('/');
+  } catch(err) {
+    console.log('Error: ', err);
+    res.render('error', { err });
+  };
 });
 
 module.exports = app;
