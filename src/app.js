af9bb113754111bad6ff498d54ece8e764de4ea9
Pedro Castilho <pedro@castilho.in>
2020-03-22 16:11:50 -0300

    Edit and update tasks

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 const path = require('path');
 const db = require(path.join(__dirname, '..', 'config', 'database'));
@@ -7,18 +8,36 @@ app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// index page
+app.use(methodOverride('_method'));
+// index - show tasks
 app.get('/', async (req, res, next) => {
   const query = await db.query("SELECT * FROM tasks");
   const tasks = query.rows;
-  return res.render('index', { tasks });
+  res.render('index', { tasks });
 });
 
-// add task
+// create task
 app.post('/', async (req, res, next) => {
   const name = req.body.newTask;
   await db.query("INSERT INTO tasks (name, completed) VALUES ($1, $2)", [name, false]);
-  return res.redirect('/');
+  res.redirect('/');
 });
+
+// edit task
+app.get('/:id/edit', async (req, res, next) => {
+  const id = req.params.id;
+  const query = await db.query("SELECT * FROM tasks WHERE id = $1", [id]);
+  const row = query.rows[0];
+  res.render('edit', { task: row });
+});
+
+// update task
+app.put('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const name = req.body.taskName;
+  const completed = (req.body.completed === 'completed') ? true : false;
+  await db.query("UPDATE tasks SET name = $2, completed = $3 WHERE id = $1", [id, name, completed]);
+  res.redirect('/');
+});
+
 module.exports = app;
